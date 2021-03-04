9df8cc8f36f4eefc076e9c2d9951c1407e9a70d2
tarun005 <tarun.05.kalluri@gmail.com>
2021-03-04 11:39:15 -0800

    Release code

--- Middleburry_Test.py
+++ Middleburry_Test.py
@@ -19,8 +19,6 @@ import myutils
 from torch.utils.data import DataLoader
 
 
-##### Parse CmdLine Arguments #####
-os.environ["CUDA_VISIBLE_DEVICES"]='1'
 args, unparsed = config.get_args()
 cwd = os.getcwd()
 
